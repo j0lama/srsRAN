03d10045108f5199d02b7471c0a9c05df5dfb3b6
Jon Larrea <jon.larrea@ed.ac.uk>
2022-07-07 12:22:34 +0100

    Changing ZMQ calls by UDP calls

--- lib/src/phy/rf/rf_udp_imp_tx.c
+++ lib/src/phy/rf/rf_udp_imp_tx.c
@@ -30,6 +30,7 @@
 #include <sys/socket.h>
 #include <arpa/inet.h>
 #include <unistd.h>
+#include <time.h>
 
 int rf_udp_tx_open(rf_udp_tx_t* q, rf_udp_opts_t opts, char* sock_args)
 {
@@ -72,13 +73,15 @@ int rf_udp_tx_open(rf_udp_tx_t* q, rf_udp_opts_t opts, char* sock_args)
     }
 
     if (opts.trx_timeout_ms) {
-      int timeout = (int) opts.trx_timeout_ms;
-      if (setsockopt(q->sock, IPPROTO_UDP, SO_RCVTIMEO, (void *) &timeout, (socklen_t) sizeof(timeout)) == -1) {
+      struct timeval tv;
+      tv.tv_sec = 0;
+      tv.tv_usec = 1000*((int) opts.trx_timeout_ms);
+      if (setsockopt(q->sock, SOL_SOCKET, SO_RCVTIMEO, (void *) &tv, (socklen_t) sizeof(tv)) == -1) {
         fprintf(stderr, "Error: setting receive timeout on tx socket (%s)\n", strerror(errno));
         goto clean_exit;
       }
 
-      if (setsockopt(q->sock, IPPROTO_UDP, SO_SNDTIMEO, (void *) &timeout, (socklen_t) sizeof(timeout)) == -1) {
+      if (setsockopt(q->sock, SOL_SOCKET, SO_SNDTIMEO, (void *) &tv, (socklen_t) sizeof(tv)) == -1) {
         fprintf(stderr, "Error: setting send timeout on tx socket (%s)\n", strerror(errno));
         goto clean_exit;
       }
@@ -86,7 +89,7 @@ int rf_udp_tx_open(rf_udp_tx_t* q, rf_udp_opts_t opts, char* sock_args)
       struct linger lin;
       lin.l_onoff = 1;
       lin.l_linger = 0;
-      if (setsockopt(q->sock, IPPROTO_UDP, SO_LINGER, (void *) &lin, (socklen_t) sizeof(lin)) == -1) {
+      if (setsockopt(q->sock, SOL_SOCKET, SO_LINGER, (void *) &lin, (socklen_t) sizeof(lin)) == -1) {
         fprintf(stderr, "Error: setting linger timeout on tx socket (%s)\n", strerror(errno));
         goto clean_exit;
       }
