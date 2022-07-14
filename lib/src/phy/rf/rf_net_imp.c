f634d6b1b87952ebb697c6fa75e3e95c000621d8
Jon Larrea <jon.larrea@ed.ac.uk>
2022-07-14 14:58:41 +0100

    Debugging

--- lib/src/phy/rf/rf_net_imp.c
+++ lib/src/phy/rf/rf_net_imp.c
@@ -626,7 +626,6 @@ int rf_net_recv_with_time_multi(void* h, void** data, uint32_t nsamples, bool bl
     // Protect the access to decim_factor since is a shared variable
     pthread_mutex_lock(&handler->decim_mutex);
     uint32_t decim_factor = handler->decim_factor;
-    uint32_t aux_decim = decim_factor;
     decim_factor = 1;
     pthread_mutex_unlock(&handler->decim_mutex);
 
@@ -746,20 +745,19 @@ int rf_net_recv_with_time_multi(void* h, void** data, uint32_t nsamples, bool bl
       }
     }
 
-    decim_factor = aux_decim;
     // Set gain
-    pthread_mutex_lock(&handler->rx_gain_mutex);
-    float scale = srsran_convert_dB_to_amplitude(handler->rx_gain);
-    pthread_mutex_unlock(&handler->rx_gain_mutex);
-    // scale shall also incorporate decim_factor
-    if (decim_factor > 0) {
-      scale = scale / decim_factor;
-    }
-    for (uint32_t c = 0; c < handler->nof_channels; c++) {
-      if (buffers[c]) {
-        srsran_vec_sc_prod_cfc(buffers[c], scale, buffers[c], nsamples);
-      }
-    }
+    //pthread_mutex_lock(&handler->rx_gain_mutex);
+    //float scale = srsran_convert_dB_to_amplitude(handler->rx_gain);
+    //pthread_mutex_unlock(&handler->rx_gain_mutex);
+    //// scale shall also incorporate decim_factor
+    //if (decim_factor > 0) {
+    //  scale = scale / decim_factor;
+    //}
+    //for (uint32_t c = 0; c < handler->nof_channels; c++) {
+    //  if (buffers[c]) {
+    //    srsran_vec_sc_prod_cfc(buffers[c], scale, buffers[c], nsamples);
+    //  }
+    //}
 
     // update rx time
     update_ts(handler, &handler->next_rx_ts, nsamples_baserate, "rx");
@@ -912,7 +910,7 @@ int rf_net_send_timed_multi(void*  h,
         }
 
         // Scale according to current gain
-        srsran_vec_sc_prod_cfc(buf, tx_gain, buf, nsamples_baseband);
+        //srsran_vec_sc_prod_cfc(buf, tx_gain, buf, nsamples_baseband);
 
         // Finally, transmit baseband
         int n = rf_net_tx_baseband(&handler->transmitter[i], buf, nsamples_baseband);
